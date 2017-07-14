1c3e533ffcbe50aef9dca3713d1232522f9a72ac
Hardik Kanjariya <myhardy8520112@gmail.com>
2017-07-14 13:03:12 +0530

    UI & Routing Added to Application

---
+++ index.js
@@ -0,0 +1,34 @@
+// var http = require('http');
+
+// var server = http.createServer(function(req, res) {
+// res.writeHead(200);
+
+
+// var iFlyChat = require('iflychat'); //load iflychat module.
+
+// let apiKey = '67d1da6f-b6ee-4483-a682-2a22e437e7ba';
+// let appId = '9ldkdjvDDWZYQ2zkLPmpWVzUDdcBooBv3DfMgM4jaXUW53017';
+
+// iFlyChat.init(apiKey, appId);
+
+// console.log("iFlyChat", iFlyChat);
+
+// var user_details = {
+//     'user_id': 'Hardik', // string (required)
+//     'user_name': 'DemonMind',  //string (required)
+//     'is_admin': true, //boolean (optional)
+//     'user_avatar_url': 'https://in.linkedin.com/in/hardik-kanjariya', // string (optional)
+//     'user_profile_url': 'https://in.linkedin.com/in/hardik-kanjariya', // string (optional)
+// };
+
+// iFlyChat.setUser(user_details); 
+
+// iFlyChat.getHtmlCode(function(error, response){
+//    console.log(response);
+// });
+
+// res.end('Hi everybody, How are you ?');
+// });
+
+// server.listen(8080);
+
